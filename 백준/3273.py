2a4db1ff31507d4aa33a54545090843a99bc6840
mickeyshoes <parody9c@gmail.com>
2022-04-16 23:31:42 +0900

    [ADD] two pointer init

---
+++ 백준/3273.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip('\n'))
+items = list(map(int, input().rstrip('\n').split()))
+items.sort()
+X = int(input().rstrip('\n'))
+start = 0
+end = len(items) - 1
+cnt = 0
+
+while start <= end:
+    value = items[start] + items[end]
+    if value > X:
+        end -=1
+    else:
+        if value == X:
+            cnt+=1
+        start +=1
+
+print(cnt)

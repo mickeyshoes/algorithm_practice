7e2358be823c53c906842d95a07fd941bb213bac
mickeyshoes <parody9c@gmail.com>
2022-04-17 23:38:49 +0900

    [FAIL] two pointer(2)

---
+++ 백준/11728.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int, input().rstrip('\n').split())
+A = list(map(int, input().rstrip('\n').split()))
+B = list(map(int, input().rstrip('\n').split()))

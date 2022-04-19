d2f750a5c5d4cbafac23cc117a26aa661f8bc8f0
mickeyshoes <parody9c@gmail.com>
2022-04-19 23:42:43 +0900

    [ADD] binary search(1)

--- 백준/10815_dic.py
+++ 백준/10815_dic.py
@@ -1,27 +1,22 @@
 import sys
+from collections import defaultdict
 input = sys.stdin.readline
 
 N = int(input().rstrip('\n'))
 ary1 = list(map(int, input().rstrip('\n').split()))
-ary1.sort()
+dic = defaultdict(bool)
 M = int(input().rstrip('\n'))
 ary2 = list(map(int, input().rstrip('\n').split()))
 answer = []
 
-for i in ary2:
-    start = 0
-    end = N -1
+for i in ary1:
+    dic[i] = True
 
-    while start <= end:
-        div = (start+end)//2
-        if i > ary1[div]:
-            start = div+1
-        elif i < ary1[div]:
-            end = div-1
-        else:
-            answer.append(1)
-            break
-    if start > end:
+for i in ary2:
+    if dic[i]:
+        answer.append(1)
+    else:
         answer.append(0)
+
 
 print(' '.join([str(i) for i in answer]))

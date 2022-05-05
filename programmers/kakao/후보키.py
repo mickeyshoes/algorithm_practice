de138166e40e063bbbf6e6938ba3ff1ecebf98e8
mickeyshoes <parody9c@gmail.com>
2022-05-05 23:06:44 +0900

    [FAIL] programmers kakao candidate key(2)

--- programmers/kakao/후보키.py
+++ programmers/kakao/후보키.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 
 def solution(relation):
+    answer = 0
     row, column = len(relation[0]), len(relation)
     idx = tuple(range(row))
     cand_key_set = set()
@@ -14,12 +15,27 @@ def solution(relation):
             print(i, comb)
             # print(unique_set)
             if len(unique_set) == column:
-                cand_key_set.add(bin(sum([2**item for item in comb])))
+                cand_key_set.add(sum([2**item for item in comb]))
     # 생성된 키를 통해 최소성을 확인하기
     '''
     위에서 비트 마스킹 한 결과로 & 연산 돌려서 하나라도 1 뜨는 원소 제거하면 되는데
     생각만큼 코드가 잘 안짜진다..
     '''
-    print(sorted(cand_key_set, key=lambda x:len(x)))
+    cand_key_set = sorted(cand_key_set)
+    # print(cand_key_set)
+    minimal_key = list()
+    start = 0
+    if cand_key_set:
+        minimal_key.append(cand_key_set[0])
+        answer +=1
+        start +=1
     
-    return len(cand_key_set)
+    while minimal_key:
+        bitwise = minimal_key.pop()
+        for i in range(start, len(cand_key_set)):
+            if bitwise & cand_key_set[i] == 0:
+                minimal_key.append(cand_key_set[i])
+                print(bin(bitwise),bin(cand_key_set[i]))
+                answer+=1
+        start +=1
+    return answer

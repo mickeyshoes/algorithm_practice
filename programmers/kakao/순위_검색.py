290b3fc6453e13fa68027914d6bb12e46618e288
mickeyshoes <parody9c@gmail.com>
2022-05-22 23:41:36 +0900

    [FAIL] programmers kakao rank search(2)

--- programmers/kakao/순위_검색.py
+++ programmers/kakao/순위_검색.py
@@ -1,4 +1,17 @@
 from collections import defaultdict
+def lower_bound(score_list:list, point:int)->set:
+    start, mid, end = 0, 0, len(score_list)
+    
+    while start <=end:
+        mid = (start+end)//2
+        if score_list[mid][0] < point:
+            start = mid +1
+        else:
+            end = mid
+    
+    target_set = set(score_list[i][1] for i in range(mid,end))
+    return target_set
+        
 def solution(info, query):
     answer = []
     # string info -> dict(set)으로
@@ -10,7 +23,8 @@ def solution(info, query):
         info_dict[job].add(idx)
         info_dict[car].add(idx)
         info_dict[food].add(idx)
-        score[idx] = int(point)
+        score[idx] = tuple(int(point), idx)
+    score.sort(key=lambda x:x[0])
         
     for q in query:
         stack = []
@@ -31,7 +45,7 @@ def solution(info, query):
                 result = result & all_pass
             elif i.isdigit():
                 target_score = int(i)
-                result = result & set(idx for idx in range(len(score)) if score[idx] >= target_score)
+                result = result & lower_bound(score, target_score)
             else:
                 result = result & info_dict[i]
         answer.append(len(result))

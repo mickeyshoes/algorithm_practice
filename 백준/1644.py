50a98c854364f2a6984c901a18e5a2c2f7d85c8e
mickeyshoes <parody9c@gmail.com>
2022-05-24 22:35:14 +0900

    [MV] Human Error mv 1325.py -> 1326.py(boj bfs)

---
+++ 백준/1644.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip('\n'))
+
+def is_prime(number:int)->bool:
+    for i in range(2, int(number**0.5)+1):
+        if number % i == 0:
+            return False
+    return True
+
+ary = [i for i in range(2,N+1) if is_prime(i)]
+cnt, end, prefix_sum = 0,0,0
+lim = len(ary)
+for i in range(lim):
+    while end != lim:
+        if prefix_sum >= N:
+            if prefix_sum == N:
+                cnt +=1
+            break
+        prefix_sum += ary[end]
+        end+=1
+    prefix_sum -= ary[i]
+
+print(cnt+1) if ary[-1] == N else print(cnt)

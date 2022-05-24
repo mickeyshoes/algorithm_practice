be16efe62b0101e2ac615373309c75f1b73bbef1
mickeyshoes <parody9c@gmail.com>
2022-05-24 10:20:34 +0900

    [ADD] boj BFS

--- 백준/1325.py
+++ 백준/1325.py
@@ -1,60 +1,26 @@
 import sys
-from collections import defaultdict,deque
+from collections import deque
 input = sys.stdin.readline
 
-N,M = map(int, input().rstrip('\n').split())
-d = defaultdict(list)
+N = int(input().rstrip('\n'))
+ary = list(map(int, input().rstrip('\n').split()))
+A,B = map(int, input().rstrip('\n').split())
 
-for _ in range(M):
-    a,b = map(int, input().rstrip('\n').split())
-    d[b].append(a)
+visited = [False] * N
 
-def bfs(start:int, d:dict, m:int):
-    q = deque([])
-    visited = [False] * m
-    q.append(start)
-    visited[start-1] = True
-    count = 0
+def BFS(start:int, end:int):
+    q= deque([(0,start)])
+
     while q:
-        node = q.popleft()
-        count +=1
-        for n in d[node]:
-            if not visited[n-1]:
-                visited[n-1] = True
-                q.append(n)
-    return count
+        depth, node = q.popleft()
+        visited[node] = True
+        
+        for i in range(N):
+            if not visited[i] and abs(node-i) % ary[node] == 0:
+                q.append((depth+1, i))
+                if i == end:
+                    return depth+1
 
-answer = []
-temp = 0
-for i in range(1,N+1):
-    if d[i]:
-        a = bfs(i,d,N)
-        if temp <= a:
-            if temp < a:
-                answer = []
-                temp = a
-            answer.append(i)
-print(*answer)
 
-# def dfs(start : int, d: dict, m : int):
-#     stack = []
-#     stack.extend(d[start])
-#     count = 0
-#     while stack:
-#         a = stack.pop()
-#         if d[a]:
-#             stack.extend(d[a])
-#         count +=1
-    
-#     return count
-
-# answer = []
-# temp = 0
-# for i in range(1,N+1):
-#     a = dfs(i,d,N)
-#     if temp <= a:
-#         if temp < a:
-#             answer = []
-#             temp = a
-#         answer.append(i)
-# print(*answer)
+answer = BFS(A-1, B-1)
+print(answer) if answer else print(-1)

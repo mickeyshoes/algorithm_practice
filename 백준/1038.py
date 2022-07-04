767c2197c2f70f4c4af5bdbeaf3141a6dc5700bd
mickeyshoes <parody9c@gmail.com>
2022-07-05 04:06:49 +0900

    [ADD] boj descending number - dp

--- 백준/1038.py
+++ 백준/1038.py
@@ -1,21 +1,41 @@
-from re import L
 import sys
+from collections import defaultdict
 input= sys.stdin.readline
 N = int(input().rstrip('\n'))
 
-items = list(range(10))
-stack =[]
+# 1. define table
+dp = defaultdict(list)
+answer = []
+# 2. init table
+def is_decending(num:str)->bool:
+    prev_value = 10
+    for i in range(len(num)):
+        target = int(num[i])
+        if target >= prev_value:
+            return False
+        prev_value = target
+    return True
 
-def DFS(idx:int, limit:int):
-    if len(stack) == limit:
-        print(*stack)
-        return
+for i in range(100):
+    target = str(i)
+    if is_decending(target):
+        dp[len(target)].append(target)
+        answer.append(i)
 
-    for i in range(idx):
-        stack.append(items[i])
-        DFS(i, limit)
-        stack.pop()
+# 3. fill table
+for i in range(3,10):
+    temp = []
+    for j in range(i-1,10):
+        for item in dp[i-1]:
+            if str(j) > item[0]:
+                temp.append(str(j)+item)
+            else:
+                break
+    dp[i] = temp
+    answer.extend(temp)
+answer.append(9876543210)
 
-
-for i in range(3):
-    DFS(,i)
+if N >= len(answer):
+    print(-1)
+else:
+    print(answer[N])

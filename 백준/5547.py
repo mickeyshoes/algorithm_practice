87a9f7281f3a8accb06539cde3b2b191012832a6
mickeyshoes <parody9c@gmail.com>
2022-07-07 23:44:20 +0900

    [FAIL] boj illumination

--- 백준/5547.py
+++ 백준/5547.py
@@ -4,5 +4,51 @@ W,H = map(int, input().split())
 
 building_map = [list(map(int, input().split())) for _ in range(H)]
 
-def move(x:int, y:int, d:int)->bool:
-    return False
+def is_range(x:int, y:int)->bool:
+    return 0<=x<H and 0<=y<W
+
+def move(x:int, y:int, d:int)->tuple:
+    is_even = y%2 == 0
+    tx,ty = 0,0
+    if d == 0:
+        ty = -1
+        if is_even:
+            tx = -1
+
+    if d == 1:
+        ty = -1
+        if not is_even:
+            tx = 1
+
+    if d == 2:
+        tx = 1
+    
+    if d == 3:
+        ty = 1
+        if not is_even:
+            tx = 1
+
+    if d == 4:
+        ty = 1
+        if is_even:
+            tx = -1
+    
+    if d == 5:
+        tx = -1
+
+    return (tx,ty)
+
+answer = 0
+for i in range(H):
+    for j in range(W):
+        if building_map[i][j] == 1:
+            cnt = 0
+            for k in range(6):
+                tx,ty = move(i,j,k)
+                if is_range(tx,ty) and building_map[tx][ty] ==0:
+                    cnt +=1
+                if not is_range(tx,ty):
+                    cnt +=1
+            answer += cnt
+
+print(answer)
